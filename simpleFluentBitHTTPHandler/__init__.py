9175233a4f93d3766e09e6fd4749aa9a9cded76e
Matthieu Huin <mhuin@redhat.com>
2023-11-07 17:30:44 +0100

    Rename handler, manage special env vars

--- simpleFluentBitHTTPHandler/__init__.py
+++ simpleFluentBitHTTPHandler/__init__.py
@@ -2,20 +2,28 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+import os
 import logging
 
 import requests
 
 
-class SimpleFluentBitHTTPHandler(logging.Handler):
+class SimpleFluentBitHTTPInputHandler(logging.Handler):
+    """A minimal handler for sending logs to the HTTP Input
+    of a Fluent Bit collector."""
     def __init__(self, url):
         logging.Handler.__init__(self)
         self.url = url
+
 
     def emit(self, record):
         d = {
             'log': self.format(record)
         }
+        for envvar in os.environ:
+            if envvar.startswith('K8S_'):
+                key = envvar[4:].lower().replace('_', '.')
+                d[key] = os.environ[envvar]
         try:
             req = requests.post(self.url, json=d)
             req.raise_for_status()
