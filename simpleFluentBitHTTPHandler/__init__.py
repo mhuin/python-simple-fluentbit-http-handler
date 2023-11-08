828a72c6606b490460affa0e6c1fe017b3636b1f
Matthieu Huin <mhuin@redhat.com>
2023-11-08 10:44:22 +0100

    make env prefix optional

--- simpleFluentBitHTTPHandler/__init__.py
+++ simpleFluentBitHTTPHandler/__init__.py
@@ -11,19 +11,20 @@ import requests
 class SimpleFluentBitHTTPInputHandler(logging.Handler):
     """A minimal handler for sending logs to the HTTP Input
     of a Fluent Bit collector."""
-    def __init__(self, url):
+    def __init__(self, url, env_prefix=None):
         logging.Handler.__init__(self)
         self.url = url
-
+        self.env_prefix = env_prefix
 
     def emit(self, record):
         d = {
             'log': self.format(record)
         }
-        for envvar in os.environ:
-            if envvar.startswith('K8S_'):
-                key = envvar[4:].lower().replace('_', '.')
-                d[key] = os.environ[envvar]
+        if self.env_prefix is not None:
+            for envvar in os.environ:
+                if envvar.startswith(self.env_prefix):
+                    key = envvar[len(self.env_prefix):].lower()
+                    d[key] = os.environ[envvar]
         try:
             req = requests.post(self.url, json=d)
             req.raise_for_status()
